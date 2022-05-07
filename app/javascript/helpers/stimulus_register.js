22a8d238c5c7caf717bb782a1b36d492981a4fb5
altarasov <altarasov@rnds.pro>
2022-05-07 17:09:57 +0300

    refactor/configuration app

--- app/javascript/helpers/stimulus_register.js
+++ app/javascript/helpers/stimulus_register.js
@@ -1,9 +1,9 @@
 import { Application } from "@hotwired/stimulus"
+import { definitions } from 'stimulus:../controllers'
 
 const application = Application.start()
+application.load(definitions)
 
 // Configure Stimulus development experience
 application.debug = false
-window.Stimulus   = application
-
-export { application }
+window.Stimulus = application
